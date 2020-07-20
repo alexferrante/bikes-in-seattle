0ef0d802da31f6123775f582be2e0c3d32896aaf
alexferrante <36389437+alexferrante@users.noreply.github.com>
2020-07-20 13:58:00 -0700

    Add plots

--- src/main.py
+++ src/main.py
@@ -13,11 +13,12 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 from folium.plugins import TimestampedGeoJson, HeatMapWithTime
 from constants import bike_count_locations, seattle_coords, min_date, max_date
-from analysis import Plot
+from analysis import plot_all_bike_count_data_over_time, plot_businesses_over_time
 
 __author__ = "Your Name"
 __version__ = "0.1.0"
 __license__ = "MIT"
+
 
 def prep_business_dataset():
     data_path = f"{os.getcwd()}/data"
@@ -43,12 +44,15 @@ def prep_business_dataset():
         print("Finished populating coordinate lookup")
         df.to_csv(f"{data_path}/Active_Business_Data_Modified.csv", sep="\t", index=False)
 
+
 def prep_bike_count_datasets():
     data_path = f"{os.getcwd()}/data"
     list_of_all_loc_data = []
+    list_of_bike_data_frames = []
     for loc in bike_count_locations:
-        list_of_loc_data = get_bike_data_frame(file=bike_count_locations[loc]["file_name"], data_path=data_path, coordinates=bike_count_locations[loc]["coordinates"], min_date=min_date, excess_columns=bike_count_locations[loc]["excess_cols"], comb_cols=bike_count_locations[loc]["comb_cols"])
+        list_of_loc_data, loc_df = get_bike_data_frame(file=bike_count_locations[loc]["file_name"], data_path=data_path, coordinates=bike_count_locations[loc]["coordinates"], min_date=min_date, excess_columns=bike_count_locations[loc]["excess_cols"], comb_cols=bike_count_locations[loc]["comb_cols"])
         list_of_all_loc_data.append(list_of_loc_data)
+        list_of_bike_data_frames.append({bike_count_locations[loc]["file_name"]: loc_df})
     final_data = []
     for i, _ in enumerate(list_of_all_loc_data[0]):
         list_of_period_data = []
@@ -57,12 +61,14 @@ def prep_bike_count_datasets():
         final_data.append(list_of_period_data)
     date_range = pd.date_range(min_date, max_date, freq="D")
     date_indices = [str(x) for x in date_range]
-    return final_data, date_indices
+    return final_data, date_indices, list_of_bike_data_frames
+
 
 def combine_loc_count_data_transf(r, coordinates, list_of_data_list):
     data_list = coordinates.copy()
     data_list.append(r["Total"])
     list_of_data_list.append(data_list)
+
 
 def get_bike_data_frame(file, data_path, coordinates, min_date, excess_columns, comb_cols):
     list_of_data_list = []
@@ -76,10 +82,9 @@ def get_bike_data_frame(file, data_path, coordinates, min_date, excess_columns, 
         df = df.resample("D").sum()
         df = df[df.index >= min_date]
         df.rename(columns={df.columns[0]: "Total"}, inplace=True)
-        bike_data_plot = Plot()
-        bike_data_plot.plot_bike_count_over_time(file, df)
         df.apply(combine_loc_count_data_transf, args=(coordinates, list_of_data_list), axis=1)
-        return list_of_data_list
+        return list_of_data_list, df
+
 
 def get_geojson_features(df):
     features = []
@@ -102,19 +107,17 @@ def get_geojson_features(df):
         features.append(feature)
     return features
 
+
 def main():
     """ Main entry point of the app """
-    folium_map = folium.Map(location=seattle_coords, min_zoom=13, max_bounds=True)
-    # for loc in bike_count_locations:
-    #     marker = folium.CircleMarker(location=bike_count_locations[loc]["coordinates"])
-    #     marker.add_to(folium_map)
+    bike_count_heat_map = folium.Map(location=seattle_coords, min_zoom=13, max_bounds=True)
     data_path = f"{os.getcwd()}/data"
     if not os.path.exists(f"{data_path}/Active_Business_Data_Modified.csv"):
         prep_business_dataset()
-    df = pd.read_csv(f"{data_path}/Active_Business_Data_Modified.csv", sep="\t")
-    bike_data_by_date, date_indices = prep_bike_count_datasets()
-    
-    folium_map.add_child(HeatMapWithTime(data=bike_data_by_date, 
+    business_data_frame = pd.read_csv(f"{data_path}/Active_Business_Data_Modified.csv", sep="\t")
+
+    bike_data_by_date, date_indices, bike_data_frames_list = prep_bike_count_datasets()
+    bike_count_heat_map.add_child(HeatMapWithTime(data=bike_data_by_date, 
                     index=date_indices,
                     radius=50, 
                     gradient={0.2: 'blue', 0.4: 'lime', 0.6: 'orange', 1: 'red'}, 
@@ -122,10 +125,11 @@ def main():
                     min_opacity=0.5,
                     max_opacity=0.8
     ))
+    plot_all_bike_count_data_over_time(bike_data_frames_list)
 
-    folium_map_2 = folium.Map(location=seattle_coords, min_zoom=13, max_bounds=True)
-    features = get_geojson_features(df)
-    folium_map_2.add_child(TimestampedGeoJson(
+    business_time_series_map = folium.Map(location=seattle_coords, min_zoom=13, max_bounds=True)
+    features = get_geojson_features(business_data_frame)
+    business_time_series_map.add_child(TimestampedGeoJson(
         {'type': 'FeatureCollection',
         'features': features}
         , period='P1D'
@@ -134,10 +138,13 @@ def main():
         , loop=False
         , loop_button=True
         , date_options='MM/DD/YYYY'
-        , duration="P1M"
+        , duration="P1D"
     ))
-    folium_map.save("test.html")
-    folium_map_2.save("test_2.html")
+    df2 = business_data_frame.groupby(["License Start Date"]).count()
+    plot_businesses_over_time(df2)
+    bike_count_heat_map.save("bike_count_heat_map.html")
+    business_time_series_map.save("business_time_series_map.html")
+
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
